537d0a15048260a359f9d660e73f8c72670d79cd
smn1215 <144471819+smn1215@users.noreply.github.com>
2026-02-23 23:51:55 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (10.39%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -4,7 +4,7 @@ public:
         vector<vector<int>> r(numRows);
     for (int i = 0; i < numRows; i++) {
         r[i].resize(i + 1);
-        r[i][0] = r[i][i] = 1; // Ends are always 1
+        r[i][0] = r[i][i] = 1; 
         for (int j = 1; j < i; j++)
             r[i][j] = r[i - 1][j - 1] + r[i - 1][j];
     }
