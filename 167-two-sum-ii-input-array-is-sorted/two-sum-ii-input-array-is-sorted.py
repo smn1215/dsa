7031d14724bc76ff1f6615d2d9a565bffdcc94d9
smn1215 <144471819+smn1215@users.noreply.github.com>
2026-07-11 23:12:28 +0530

    Time: 6 ms (57.49%) | Memory: 13.6 MB (6.22%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,16 +1,11 @@
-class Solution:
+class Solution(object):
     def twoSum(self, numbers, target):
-        left, right = 0, len(numbers) - 1  # two pointers
+        mp = defaultdict(int)
+        for i in range(len(numbers)):
+            tmp = target - numbers[i]
+            if mp[tmp]:
+                return [mp[tmp], i + 1]
+            mp[numbers[i]] = i + 1
+        return []
         
-        while left < right:
-            curr_sum = numbers[left] + numbers[right]
-            
-            if curr_sum == target:
-                # Return 1-indexed positions
-                return [left + 1, right + 1]
-            elif curr_sum < target:
-                left += 1   # need a larger sum
-            else:
-                right -= 1  # need a smaller sum
-
         
